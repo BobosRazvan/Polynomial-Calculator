9301d0bfb59054b6ff3d5ad3565b237a9e936705
Razvan <bobosrazvan@gmail.com>
2024-03-03 16:59:23 +0200

    front-end version1

--- src/main/java/com/example/pt2024_30425_bobos_razvanandrei_assigment_1/HelloApplication.java
+++ src/main/java/com/example/pt2024_30425_bobos_razvanandrei_assigment_1/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.pt2024_30425_bobos_razvanandrei_assigment_1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 551, 595);
+        stage.setTitle("Polynomial Calculator");
+        Image image = new Image("file:///C:/Users/danie/Desktop/Razvi/Altele/icon2.png");
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
