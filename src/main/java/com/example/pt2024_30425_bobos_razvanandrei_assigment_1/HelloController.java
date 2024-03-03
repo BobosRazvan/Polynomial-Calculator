a7c353c09a4f4f451bfc60ecb86a3bf7b80665c3
Razvan <bobosrazvan@gmail.com>
2024-03-03 15:00:02 +0200

    inital commit

---
+++ src/main/java/com/example/pt2024_30425_bobos_razvanandrei_assigment_1/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.pt2024_30425_bobos_razvanandrei_assigment_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.pt2024_30425_bobos_razvanandrei_assigment_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pt2024_30425_bobos_razvanandrei_assigment_1 to javafx.fxml;
+    exports com.example.pt2024_30425_bobos_razvanandrei_assigment_1;
+}
